aaefe808942e1734f98d965de1a52edc339108b2
martingci <m.carrasco29@ufromail.cl>
2025-08-19 16:03:48 -0400

    se añade logo y mejoras estéticas.

--- streamlit.py
+++ streamlit.py
@@ -2,16 +2,26 @@ import streamlit as st
 import qr_code_creator as maker
 import io
 
+st.logo("logo.png", size="large")
+
+
 st.title("Generador QR Sindicato")
 
 link = st.text_input("Ingrese URL")
-create = st.button("Crear QR")
+create = st.button("Crear QR", type="primary")
 if (create == 1) and link != "":
-    qr = maker.create_qrcode(link, "qr_code", "logo.png")
-    st.image(qr, caption=f'Código QR de "' + link+'"')
-    buf = io.BytesIO()
-    qr.save(buf, format="PNG")
+    with st.spinner("Creando código QR..."):
+        # Crea el código QR
+        qr = maker.create_qrcode(link, "qr_code", "logo.png")
+        buf = io.BytesIO()
+        qr.save(buf, format="PNG")
+        st.image(qr, caption=f'Código QR de "' + link+'"')
+
+    st.success("Código QR creado exitosamente!")
     st.download_button(label="Descargar imagen", data=buf.getvalue(), file_name="qr_code.png")
+
+    
+
     
     
 
